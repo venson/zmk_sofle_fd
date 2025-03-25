adba38b94c322899fd2fc1d8999b78f516d6a3bc
venson <venson.hunt@gmail.com>
2025-03-25 08:16:30 +0800

    Update board.cmake

--- boards/shields/sofle/board.cmake
+++ boards/shields/sofle/board.cmake
@@ -1,7 +1,3 @@
 # SPDX-License-Identifier: MIT
 
-board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
-board_runner_args(jlink "--device=rp2040" "--speed=4000")
-
-include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
